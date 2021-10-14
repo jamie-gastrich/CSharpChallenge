38433ee3c999df3863af493232bb0ebf47be7fa7
jamie-gastrich <jamie.gastrich@gmail.com>
2021-10-13 23:16:18 -0400

    Add files via upload

---
+++ ArrayApp.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ArrayApp
+{
+    class ArrayApp
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            int Below = 0;
+            int Above = 0;
+            int num = 0;
+            int[] numArray = { 1, 5, 2, 1, 10 };
+            char UIArray;
+
+            //Prompt the user if they want to enter their own array
+            Console.WriteLine("Enter your own Array? (Y/N)");
+            UIArray = char.Parse(Console.ReadLine());
+
+            if (UIArray == 'Y' || UIArray == 'y')
+            {
+                //Prompt the user to input their array
+                Console.WriteLine("Enter a 5 digit long array");
+
+                //Set up user input array
+                for (int i = 0; i < 5; i++)
+                {
+                    numArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                //Propt the user to enter the number they want to test
+                Console.WriteLine("Enter a number to see how many are above and below it in the array");
+                try
+                {
+                    //Set user input number
+                    num = int.Parse(Console.ReadLine());
+
+                    //Figure out count for Above and Below 
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (numArray[i] > num)
+                        {
+                            Above++;
+                        }
+                        else if (numArray[i] < num)
+                        {
+                            Below++;
+                        }
+                    }
+
+                    //Output Above, Below and the Array
+                    Console.WriteLine("Above: " + Above);
+                    Console.WriteLine("Below: " + Below);
+                    Console.WriteLine("Array = [" + numArray[0] + ", " + numArray[1] + ", " + numArray[2] + ", " + numArray[3] + ", " + numArray[4] + "]");
+
+                }
+                //If the user inputs anything other than an integer, it will fail with this note
+                catch
+                {
+                    Console.WriteLine("Please enter an integer");
+                }
+            }
+            else if (UIArray == 'N' || UIArray == 'n')
+            {
+                //Propt the user to enter the number they want to test
+                Console.WriteLine("Enter a number to see how many are above and below it in the array");
+                
+                try
+                {
+                    //Set user input number
+                    num = int.Parse(Console.ReadLine());
+
+                    //Figure out count for Above and Below 
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (numArray[i] > num)
+                        {
+                            Above++;
+                        }
+                        else if (numArray[i] < num)
+                        {
+                            Below++;
+                        }
+                    }
+
+                    //Output Above, Below and the Array
+                    Console.WriteLine("Above: " + Above);
+                    Console.WriteLine("Below: " + Below);
+                    Console.WriteLine("Array = [" + numArray[0] + ", " + numArray[1] + ", " + numArray[2] + ", " + numArray[3] + ", " + numArray[4] + "]");
+
+                }
+                //If the user inputs anything other than an integer, it will fail with this note
+                catch
+                {
+                    Console.WriteLine("Please enter an integer");
+                }
+            }
+            //If the user inputs anything other than a y or n 
+            else
+            {
+                Console.WriteLine("Please enter either a Y for yes or N for no");
+            }
+        }
+    }
+}
---
+++ RotateCharApp.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace RotateCharApp
+{
+    class RotateCharApp
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            int num = 0;
+            string RotateMe = "MyString";
+            string MeRotate;
+            char UIString;
+
+            //Prompt the user if they want to enter their own array
+            Console.WriteLine("Enter your own String? (Y/N)");
+            UIString = char.Parse(Console.ReadLine());
+
+            if (UIString == 'Y' || UIString == 'y')
+            {
+                //Prompt the user to input their array
+                Console.WriteLine("Enter the string you want to rotate");
+
+                //Set up user input array
+                RotateMe = Console.ReadLine();
+
+                //Show the string and tell the user the max integer they can input
+                Console.WriteLine("The string is \"" + RotateMe + "\" " + "and the highest integer you can input is " + RotateMe.Length);
+
+                //Propt the user to enter the integer they want to test
+                Console.WriteLine("Enter the integer you want to rotate the string on");
+                try
+                {
+                    //Set user input integer
+                    num = int.Parse(Console.ReadLine());
+
+                    //Rotate the characters in the string
+                    MeRotate = string.Format("{0}{1}", RotateMe.Substring(RotateMe.Length - num), RotateMe.Substring(0, RotateMe.Length - num));
+
+                    //Output Above, Below and the Array
+                    Console.WriteLine("Original String: " + RotateMe);
+                    Console.WriteLine("Rotated String: " + MeRotate);
+
+                }
+                //If the user inputs anything other than an integer or an integer greater than the string length, it will fail with this note
+                catch
+                {
+                    if (num > RotateMe.Length)
+                    {
+                        Console.WriteLine("Please enter an integer that is less than the string length next time");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an integer next time");
+                    }
+                }
+            }
+            else if (UIString == 'N' || UIString == 'n')
+            {
+                //Show the string and tell the user the max integer they can input
+                Console.WriteLine("The string is \"" + RotateMe + "\" " + "and the highest integer you can input is " + RotateMe.Length);
+
+                //Propt the user to enter the integer they want to test
+                Console.WriteLine("Enter the integer you want to rotate the string on");
+
+                try
+                {
+                    //Set user input integer
+                    num = int.Parse(Console.ReadLine());
+
+                    //Rotate the characters in the string
+                    MeRotate = string.Format("{0}{1}", RotateMe.Substring(RotateMe.Length - num), RotateMe.Substring(0, RotateMe.Length - num));
+
+                    //Output Above, Below and the Array
+                    Console.WriteLine("Original String: " + RotateMe);
+                    Console.WriteLine("Rotated String: " + MeRotate);
+
+                }
+                //If the user inputs anything other than an integer or an integer greater than the string length, it will fail with this note
+                catch
+                {
+                    if (num > RotateMe.Length)
+                    {
+                        Console.WriteLine("Please enter an integer that is less than the string length next time");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an integer next time");
+                    }
+                }
+            }
+            //If the user inputs anything other than a y or n 
+            else
+            {
+                Console.WriteLine("Please enter either a Y for yes or N for no next time");
+            }
+        }
+    }
+}
